197cb2268753c40dd40cbfe46f4860458011baa6
JustDaire <11017338+JustDaire@users.noreply.github.com>
2023-08-15 10:40:21 +0000

    4 files modified, 3 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import './style.css';
-import Toolbar from './toolbar'; //Add new import
+import Toolbar from './toolbar';
+import Sidebar from './parts/sidebar';
+import Footer from './parts/footer';
+
+import ReactDOM = require('react-dom');
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+  faCheckSquare,
+  faCoffee,
+  faHouse,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faCheckSquare, faCoffee, faHouse);
 
 export default function App() {
   return (
     <div>
       <Toolbar />
+      <Sidebar />
+      <Footer />
     </div>
   );
 }
---
+++ src/parts/footer.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Footer() {
+  return (
+    <div className="container">
+      <footer
+        className="d-flex flex-wrap justify-content-between align-items-center p-3 my-0 border-top fixed-bottom"
+        bg="dark"
+      >
+        <p className="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
+
+        <a
+          href="/"
+          className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        >
+          <img
+            alt=""
+            src="https://react-bootstrap.github.io/img/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+        </a>
+
+        <ul className="nav col-md-4 justify-content-end">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-body-secondary">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-body-secondary">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-body-secondary">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-body-secondary">
+              FAQs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-body-secondary">
+              About
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+}
