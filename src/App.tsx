8152398226f5a79284b2b23cdd3b9d6c7a2583d5
JustDaire <11017338+JustDaire@users.noreply.github.com>
2023-08-22 21:08:42 +0100

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -42,145 +42,150 @@ export default function App() {
         </Navbar.Collapse>
         {/* </Container> */}
       </Navbar>
-      <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-        <div
-          className="offcanvas-md offcanvas-end bg-body-tertiary"
-          tabIndex={-1}
-          id="sidebarMenu"
-          aria-labelledby="sidebarMenuLabel"
-        >
-          <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-            <ul className="nav flex-column">
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2 active"
-                  aria-current="page"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} fixedWidth />
-                  Dashboard
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faFile} fixedWidth />
-                  Orders
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Products
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Customers
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Reports
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  <FontAwesomeIcon icon={['far', 'coffee']} />
-                  Integrations
-                </a>
-              </li>
-            </ul>
+      <div className="row">
+        <div className="sidebar border border-right col-md-3 col-lg-2 p-0 ps-3 bg-body-tertiary">
+          <div
+            className="offcanvas-md offcanvas-end bg-body-tertiary"
+            tabIndex={-1}
+            id="sidebarMenu"
+            aria-labelledby="sidebarMenuLabel"
+          >
+            <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2 active"
+                    aria-current="page"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} fixedWidth />
+                    Dashboard
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faFile} fixedWidth />
+                    Orders
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Products
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Customers
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Reports
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    <FontAwesomeIcon icon={['far', 'coffee']} />
+                    Integrations
+                  </a>
+                </li>
+              </ul>
 
-            <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-              <span>Saved reports</span>
-              <a
-                className="link-secondary"
-                href="#"
-                aria-label="Add a new report"
-              >
-                <FontAwesomeIcon icon={faCirclePlus} />
-              </a>
-            </h6>
-            <ul className="nav flex-column mb-auto">
-              <li className="nav-item">
+              <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+                <span>Saved reports</span>
                 <a
-                  className="nav-link d-flex align-items-center gap-2"
+                  className="link-secondary"
                   href="#"
+                  aria-label="Add a new report"
                 >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Current month
+                  <FontAwesomeIcon icon={faCirclePlus} />
                 </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Last quarter
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Social engagement
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Year-end sale
-                </a>
-              </li>
-            </ul>
+              </h6>
+              <ul className="nav flex-column mb-auto">
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Current month
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Last quarter
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Social engagement
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Year-end sale
+                  </a>
+                </li>
+              </ul>
 
-            <hr className="my-3" />
+              <hr className="my-3" />
 
-            <ul className="nav flex-column mb-auto">
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Settings
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link d-flex align-items-center gap-2"
-                  href="#"
-                >
-                  <FontAwesomeIcon icon={faCoffee} />
-                  Sign out
-                </a>
-              </li>
-            </ul>
+              <ul className="nav flex-column mb-auto">
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Settings
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link d-flex align-items-center gap-2"
+                    href="#"
+                  >
+                    <FontAwesomeIcon icon={faCoffee} />
+                    Sign out
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
+        </div>
+        <div className="main col-md-9 col-lg-10 p-0">
+          <h1>Dashboard </h1>
         </div>
       </div>
     </div>
