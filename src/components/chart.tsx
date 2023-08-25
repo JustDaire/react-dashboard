b8e3bdf67a32cf6711a8af5232839698ff33765b
JustDaire <11017338+JustDaire@users.noreply.github.com>
2023-08-25 17:49:05 +0100

    2 files modified

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,61 +1,28 @@
 import * as React from 'react';
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from 'recharts';
+import { VictoryChart, VictoryLine, VictoryTheme } from 'victory';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+const chartdata = [
+  { x: 1, y: 2 },
+  { x: 2, y: 3 },
+  { x: 3, y: 5 },
+  { x: 4, y: 4 },
+  { x: 5, y: 7 },
 ];
 
 const Chart = () => {
-  return <>Hi</>;
+  return (
+    <>
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine
+          style={{
+            data: { stroke: '#c43a31' },
+            parent: { border: '1px solid #ccc' },
+          }}
+          data={chartdata}
+        />
+      </VictoryChart>
+    </>
+  );
 };
 
 export default Chart;
